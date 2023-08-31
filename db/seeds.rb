3cacec6424efffc0738a936d0f79904411b6739d
EC2 Default User <ec2-user@ip-172-31-27-82.ap-northeast-1.compute.internal>
2023-08-31 13:31:06 +0000

    管理者ログインボタン、オーナーログインボタンの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 管理者テストデータ
 if Admin.find_by(email: 'admin@admin.com').nil?
     Admin.create!(
         email: 'admin@admin.com',
         password: "admin123")
 end
+
