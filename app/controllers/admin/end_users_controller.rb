7dcd9b09b571f8500f13a96c058d0bfbe95559ec
EC2 Default User <ec2-user@ip-172-31-27-82.ap-northeast-1.compute.internal>
2023-09-16 13:38:34 +0000

    キャンセルのアクセス制限

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::EndUsersController < ApplicationController
   before_action :authenticate_admin
-  
+
   def index
     @end_users = EndUser.all
     @end_users = EndUser.all.page(params[:page])
@@ -13,13 +13,24 @@ class Admin::EndUsersController < ApplicationController
   def edit
     @end_user = EndUser.find(params[:id])
   end
-  
+
   def update
     @end_user = EndUser.find(params[:id])
     @end_user.update(end_user_params)
     redirect_to admin_end_user_path(params[:id])
   end
-  
+
+  def unsubscribe
+    @end_user = EndUser.find(params[:end_user_id])
+  end
+
+  def withdrawal
+    @end_user = EndUser.find(params[:end_user_id])
+    @end_user.update(is_deleted: true)
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to admin_end_users_path
+  end
+
   private
 
   def authenticate_admin
@@ -27,9 +38,9 @@ class Admin::EndUsersController < ApplicationController
       redirect_to new_admin_session_path
     end
   end
-  
+
   def end_user_params
     params.require(:end_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :sex, :birthday, :email, :telephone_number, :postal_code, :address)
   end
-  
+
 end
