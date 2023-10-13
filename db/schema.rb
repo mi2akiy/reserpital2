53acebe3ccfec6c90b69027142783773d4e14b2d
EC2 Default User <ec2-user@ip-172-31-27-82.ap-northeast-1.compute.internal>
2023-10-13 04:24:26 +0000

    seedの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,143 +10,143 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_230_720_121_259) do
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
-                                                    unique: true
+ActiveRecord::Schema.define(version: 2023_07_20_121259) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'admins', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_admins_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_admins_on_reset_password_token', unique: true
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table 'clinical_department_managers', force: :cascade do |t|
-    t.integer 'hospital_id', null: false
-    t.integer 'clinical_department_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "clinical_department_managers", force: :cascade do |t|
+    t.integer "hospital_id", null: false
+    t.integer "clinical_department_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'clinical_departments', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "clinical_departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'end_users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'last_name', null: false
-    t.string 'first_name', null: false
-    t.string 'last_name_kana', null: false
-    t.string 'first_name_kana', null: false
-    t.date 'birthday', null: false
-    t.string 'sex', null: false
-    t.string 'telephone_number', null: false
-    t.string 'postal_code', null: false
-    t.string 'address', null: false
-    t.boolean 'is_deleted', default: false, null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_end_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_end_users_on_reset_password_token', unique: true
+  create_table "end_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birthday", null: false
+    t.string "sex", null: false
+    t.string "telephone_number", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.boolean "is_deleted", default: false, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_end_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
 
-  create_table 'favorites', force: :cascade do |t|
-    t.integer 'hospital_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "favorites", force: :cascade do |t|
+    t.integer "hospital_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'hospitals', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'telephone_number', null: false
-    t.string 'postal_code', null: false
-    t.string 'address', null: false
-    t.time 'am_start_time', null: false
-    t.time 'am_end_time', null: false
-    t.time 'pm_start_time', null: false
-    t.time 'pm_end_time', null: false
-    t.string 'monday_am', null: false
-    t.string 'monday_pm', null: false
-    t.string 'tuesday_am', null: false
-    t.string 'tuesday_pm', null: false
-    t.string 'wednesday_am', null: false
-    t.string 'wednesday_pm', null: false
-    t.string 'thursday_am', null: false
-    t.string 'thursday_pm', null: false
-    t.string 'friday_am', null: false
-    t.string 'friday_pm', null: false
-    t.string 'saturday_am', null: false
-    t.string 'saturday_pm', null: false
-    t.string 'sunday_am', null: false
-    t.string 'sunday_pm', null: false
-    t.string 'holiday_am', null: false
-    t.string 'holiday_pm', null: false
-    t.string 'home_page'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "hospitals", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "telephone_number", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.time "am_start_time", null: false
+    t.time "am_end_time", null: false
+    t.time "pm_start_time", null: false
+    t.time "pm_end_time", null: false
+    t.string "monday_am", null: false
+    t.string "monday_pm", null: false
+    t.string "tuesday_am", null: false
+    t.string "tuesday_pm", null: false
+    t.string "wednesday_am", null: false
+    t.string "wednesday_pm", null: false
+    t.string "thursday_am", null: false
+    t.string "thursday_pm", null: false
+    t.string "friday_am", null: false
+    t.string "friday_pm", null: false
+    t.string "saturday_am", null: false
+    t.string "saturday_pm", null: false
+    t.string "sunday_am", null: false
+    t.string "sunday_pm", null: false
+    t.string "holiday_am", null: false
+    t.string "holiday_pm", null: false
+    t.string "home_page"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'owners', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'name'
-    t.integer 'hospital_id'
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_owners_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_owners_on_reset_password_token', unique: true
+  create_table "owners", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.integer "hospital_id"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_owners_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
   end
 
-  create_table 'reservations', force: :cascade do |t|
-    t.integer 'end_user_id', null: false
-    t.integer 'hospital_id', null: false
-    t.date 'date', null: false
-    t.time 'time', null: false
-    t.integer 'status', default: 0, null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "reservations", force: :cascade do |t|
+    t.integer "end_user_id", null: false
+    t.integer "hospital_id", null: false
+    t.date "date", null: false
+    t.time "time", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
