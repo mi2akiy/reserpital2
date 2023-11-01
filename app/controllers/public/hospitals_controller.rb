008bfb90cb554d545e8aa44e6f44d2cab40995e3
EC2 Default User <ec2-user@ip-172-31-27-82.ap-northeast-1.compute.internal>
2023-11-01 15:55:47 +0000

    病院一覧のレスポンシブ化

--- app/controllers/public/hospitals_controller.rb
+++ app/controllers/public/hospitals_controller.rb
@@ -1,13 +1,9 @@
 class Public::HospitalsController < ApplicationController
   def index
-    @hospitals = Hospital.all
-    @hospitals = Hospital.all.page(params[:page]).per(10)
-
+    @hospitals = Hospital.all.page(params[:page]).per(5)
+  
     @q = Hospital.ransack(params[:q])
     @hospitals = @q.result(distinct: true).includes(:clinical_departments).page(params[:page]).order('created_at desc')
-    return unless @q_header
-
-    @hospitals = @q_header.result(distinct: true).includes(:clinical_departments).page(params[:page]).order('created_at desc')
   end
 
   def show
